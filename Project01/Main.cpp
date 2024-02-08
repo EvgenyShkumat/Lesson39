8dfc8188f796692db8adf89e748cb095c298d85c
ITSTEP\st <st>
2024-02-08 17:20:48 +0300

    initial project

--- Project01/Main.cpp
+++ Project01/Main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
 bool is_number_prime(int number); 
 
 int main() {
 
-	cout << is_number_prime(7);
+	double now = time(0);
+
+	cout << is_number_prime(7) << endl;
+
+	now -= time(0);
+	cout << now;
 
 	return 0;
 }
